6eede48497d45c049754ded77462d1dbc4f0ea52
Thomas Krokos <tommyk101@yahoo.com>
2023-06-27 00:48:38 -0500

    Aligned the calculator better

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import Button from "./Button";
+import { flushSync } from "react-dom";
 
 const Calculator = () => {
   //want to try to move these styles out to a css file
   const container = {
     border: "1px solid",
-    borderRadius: "20%",
-    padding: "5px",
-    // display: "flex",
-    // flexWrap: "wrap",
+    borderRadius: "15%",
+    padding: "5%",
+    width: "30%",
+    height: "40%",
+    margin: "0 auto"
   };
+  const buttonContainer = {
+    display: "flex",
+    flexWrap: "wrap"
+
+  }
   const button = {
     border: "1px solid",
     borderRadius: "15%",
     padding: "25px",
+    margin: "10px"
   };
+
+  const inputField = {
+    border: "1px solid",
+    padding: "10px",
+
+
+  }
 
   const buttonValues = [
     "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "/", "0", ".", "=", "X",
@@ -22,7 +37,10 @@ const Calculator = () => {
 
   return (
     <div style={container}>
-      <div>
+      <div style={inputField}>
+        <p > Text Here</p>
+      </div>
+      <div style={buttonContainer}>
       {buttonValues.map((item) => {
          return <div style={button}>{item}</div>
       })}
