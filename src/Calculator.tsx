48b0c396a03e827d5b4e48a5ec147d15a027dd3c
Thomas Krokos <tommyk101@yahoo.com>
2023-06-30 15:59:40 -0500

    Added basic state

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import { flushSync } from "react-dom";
 import styles from "./Calculator.module.css"
 
 const Calculator = () => {
+
+  const [currentNum, SetCurrentNum] = useState("0")
 
   const buttonValues = [
     "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "/", "0", ".", "=", "X",
@@ -11,12 +13,12 @@ const Calculator = () => {
 
   return (
     <div className={styles.container}>
-      <div> 
-        <p className={styles.inputfield}> Numbers here</p>
+      <div > 
+        <p className={styles.inputfield}>{currentNum}</p>
       </div>
       <div className={styles.buttoncontainer}>
       {buttonValues.map((item) => {
-         return <div className={styles.button}>{item}</div>
+         return <div className={styles.button} onClick={() => SetCurrentNum(item)}>{item}</div>
       })}
       </div>
 
