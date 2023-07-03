5398a8e5d26da28134c0f8fd3405bf90cac55d3a
Thomas Krokos <tommyk101@yahoo.com>
2023-07-02 22:40:20 -0500

    Wrote skeleton logic for calculator

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -4,25 +4,74 @@ import styles from "./Calculator.module.css";
 
 // TODO:
 // Use ASCII values to parse what type of button is pressed
-// Use Stack to store currentNum, operator and inputNum
-// Name these numbers better
 // Write function that computes the stack and updates value of current number
 // Need to call this function when = is pressed and when second operator is pressed
 // Error handling for pressing multiple operators in a row?
-// Making the inputField able to be typed in
-// NEED A WAY TO CHECK FOR MULTIPLE . IN NUM
+// Making the inputField able to be typed in  -- lategame
 
+// ERRORS TO CHECK
+// NEED A WAY TO CHECK FOR MULTIPLE . IN NUM
+// NEED A WAY TO CHECK FOR BACK TO BACK OPERATOR PRESSES
 const Calculator = () => {
   const [currentNum, SetCurrentNum] = useState("0");
 
-
-
-// numA and numB will be stored as strings except when executing operation
+  // numA and numB will be stored as strings except when executing operation
   const [numA, SetNumA] = useState("0");
   const [numB, SetNumB] = useState("0");
   const [op, SetOp] = useState("");
 
+  // should potentially consider exporting these functions to another file?
+  const parseButton = (buttonValue: string) => {
+    // This function will parse what button is clicked through ascii value of char
+    // return boolean to represent if button was num or operator
+    // true if op false if num
+    return 1;
+  };
 
+  // if is
+
+  const onClick = (buttonValue: string) => {
+    switch (parseButton(buttonValue)) {
+      case 1: // parseButton returns 1 if buttonValue is '='
+        runEquals(); // execute = logic
+        break;
+      case 2: // parseButton returns 2 if buttonValue is 'C'
+        clearMem(); // resets calculator memory
+        break;
+
+      case 3: // parseButton returns 3 if buttonValue is an operater: ['+', '-', '/' '*']
+        runOp(buttonValue); // execute operator logic
+        break;
+
+      default: // should only reach default if button was a number or '.'
+        updateNum(buttonValue); // appends character to currentNum in scope
+    }
+  };
+
+  const clearMem = () => {
+    // sets numA, numB, and op to initial values
+    // probable will set a clear button to this function in the future
+  };
+
+  const runEquals = () => {
+    // if op === "" run no operation and just return
+    // if op != "" && numB != "":
+    // set numA = numA op numB and keep numB the same
+    // set currentNum to numA
+    // ERROR CHECKING FOR BACK TO BACK OPERATOR OPERATOR TO BE FIGURED OUT LATER
+  };
+
+  const runOp = (op: string) => {
+    //
+    // do numA op numB and then save that to numA
+    // switch to writing to numB
+    // ERROR CHECKING FOR BACK TO BACK OPERATOR TO BE FIGURED OUT LATER
+  };
+
+  const updateNum = (numToAppend: string) => {
+    // updates current number in scope
+    // ERROR CHECKING FOR BACK TO BACK OPERATOR TO BE FIGURED OUT LATER
+  };
 
   const buttonValues = [
     "1",
